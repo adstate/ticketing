09f3ea20c83688ccbe698a84cceeef7eb3c0aede
Felix Khodakovsky <felix.khodakovsky@quantumsoft.ru>
2021-10-14 17:39:02 +1000

    improve auth service

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import { currentUser } from '../middlewars/current-user';
+import { requireAuth } from '../middlewars/require-auth';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
+router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
